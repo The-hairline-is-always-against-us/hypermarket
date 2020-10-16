02abad8b31dd197a457f1a73212b2366201588b6
caijiang <caijiang199906@163.com>
2020-10-16 09:26:01 +0800

    suibian

--- src/main/java/com/harigroup/hypermarket/controller/RegisterController.java
+++ src/main/java/com/harigroup/hypermarket/controller/RegisterController.java
@@ -1,11 +1,15 @@
 package com.harigroup.hypermarket.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.harigroup.hypermarket.mapper.IUserMapper;
 import com.harigroup.hypermarket.pojo.ResultMap;
+import com.harigroup.hypermarket.service.IUserService;
 
 /**
  * 
@@ -15,23 +19,33 @@ import com.harigroup.hypermarket.pojo.ResultMap;
 @RestController
 public class RegisterController {
 	
+	@Autowired
+	private IUserService userService;
+	@Autowired
+	private ResultMap resultMap;
+	
 	@PostMapping("/register")
 	public ResultMap register(@RequestParam("user") String user) {
 		return null;
 	}
 	
-	@GetMapping("/validate/{username}")
-	public ResultMap validateUsername(@RequestParam("username") String username) {
+	@GetMapping("/validateu/{username}")
+	public ResultMap validateUsername(@PathVariable("username") String username) {
+		Integer validateUsername = userService.validateUsername(username);
+		if (validateUsername > 0) {
+			return resultMap.fail().message("已被使用");
+		} else {
+			return resultMap.success().message("可以使用");
+		}
+	}
+	
+	@GetMapping("/validatee/{email}")
+	public ResultMap validateEmail(@PathVariable("email") String email) {
 		return null;
 	}
 	
-	@GetMapping("/validate/{email}")
-	public ResultMap validateEmail(@RequestParam("email") String email) {
-		return null;
-	}
-	
-	@GetMapping("/validate/{phone}")
-	public ResultMap validatePhone(@RequestParam("phone") String phone) {
+	@GetMapping("/validatep/{phone}")
+	public ResultMap validatePhone(@PathVariable("phone") String phone) {
 		return null;
 	}
 }
--- src/test/java/com/harigroup/hypermarket/HypermarketApplicationTests.java
+++ src/test/java/com/harigroup/hypermarket/HypermarketApplicationTests.java
@@ -1,13 +1,28 @@
 package com.harigroup.hypermarket;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.alibaba.fastjson.JSON;
+import com.harigroup.hypermarket.mapper.IUserMapper;
+import com.harigroup.hypermarket.pojo.User;
 
 @SpringBootTest
 class HypermarketApplicationTests {
 
+	@Autowired
+	IUserMapper userMapper;
+	
 	@Test
 	void contextLoads() {
+		User login = userMapper.login("root", "root");
+		
+		String json = JSON.toJSONString(login);
+		
+		User u = JSON.parseObject(json,User.class);
+		
+		
 	}
 	
 }
